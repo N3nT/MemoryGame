dbea19f4eb62d01797245a66543f983fe15d5a75
N3nT <bar55v71@interia.pl>
2025-01-25 16:09:24 +0100

    LoseAlert component finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,76 +1,92 @@
-import { useState } from 'react';
-import { CircleHelp } from 'lucide-react';
-import LoseAlert from "./components/LoseAlert";
-import WinAlert from "./components/WinAlert";
-import Card from "./components/Card";
-import Help from "./components/Help";
-
+import { useEffect, useState } from 'react'
+import { CircleHelp } from 'lucide-react'
+import LoseAlert from './components/LoseAlert'
+import WinAlert from './components/WinAlert'
+import Card from './components/Card'
+import Help from './components/Help'
 
 function App() {
-  const [showHelp, setShowHelp] = useState(false);
-  const [score, setScore] = useState(0);
-  const [prevScore, setPrevScore] = useState(0);
-  const [bestScore, setBestScore] = useState(0);
-  const [clickedCards, setClickedCards] = useState([]);
-  const [lose, setLose] = useState(0);
-  const id = [1, 5, 100, 69, 32];
+	const [showHelp, setShowHelp] = useState(false)
+	const [score, setScore] = useState(0)
+	const [prevScore, setPrevScore] = useState(0)
+	const [bestScore, setBestScore] = useState(0)
+	const [clickedCards, setClickedCards] = useState([])
+	const [lose, setLose] = useState(0)
+	const id = [1, 5, 100, 69, 32]
 
-  const shuffleArray = (array) => {
+	const shuffleArray = (array) => {
 		for (let i = array.length - 1; i >= 0; i--) {
-			const j = Math.floor(Math.random() * (i + 1));
-			[array[i], array[j]] = [array[j], array[i]];
+			const j = Math.floor(Math.random() * (i + 1))
+			;[array[i], array[j]] = [array[j], array[i]]
 		}
 	}
 
-  const handleCardClick = (id) => {
-    setLose(0);
-    setClickedCards((prev) => {
-      if (!prev.includes(id)) {
-        const u = [...prev, id];
-        console.log("Clicked Cards:", u);
-        setScore(score+1);
-        return [...prev, id];
-      }
-      loseGame();
-      return prev;
-    });
-  };
+	const handleCardClick = (id) => {
+		if (lose == 0) {
+			setLose(0)
+			setClickedCards((prev) => {
+				if (!prev.includes(id)) {
+					const u = [...prev, id]
+					console.log('Clicked Cards:', u)
+					setScore(score + 1)
+					return [...prev, id]
+				}
+				loseGame()
+				return prev
+			})
+		}
+	}
 
-  const handleHelpClick = () => {
-    setShowHelp(!showHelp);
-  }
+	const handleHelpClick = () => {
+		setShowHelp(!showHelp)
+	}
 
-  const loseGame = () => {
-    setLose(1);
-    setClickedCards([]);
-    if (score > bestScore){
-      setBestScore(score);
-    }
-    setPrevScore(score);
-    setScore(0);
-  }
+	const loseGame = () => {
+		setLose(1)
+		setClickedCards([])
+		if (score > bestScore) {
+			setBestScore(score)
+		}
+		setPrevScore(score)
+	}
 
-  shuffleArray(id);
+	const newGame = () => {
+		setLose(0)
+		setClickedCards([])
+		setScore(0)
+	}
 
-  return (
-    <div className="flex flex-col justify-center items-center h-screen bg-gradient-to-r from-indigo-700 to-blue-950 relative">
-      <h1 className="text-4xl text-white font-bold mb-5 flex items-center">Memory Game <CircleHelp className="ml-5 hover:text-black cursor-pointer" onClick={handleHelpClick}/></h1>
-      <Help display={showHelp}/>
-      <div className="flex">
-        {id.map((index) => {
-          return(
-            <Card key={index} id={index} onClick={() => handleCardClick(index)}/>
-          )
-        })}
-      </div>
-      <div className='flex text-xl text-white mt-10'>
-        <p>Score: {score}</p>
-        <p className='ml-5'>Best Score: {bestScore}</p>
-      </div>
-      <LoseAlert lose={lose} score={prevScore}/>
-      <WinAlert win={clickedCards.length == 5}/>
-    </div>
-  )
+	shuffleArray(id)
+
+	return (
+		<div className='flex flex-col justify-center items-center h-screen bg-gradient-to-r from-indigo-700 to-blue-950 relative'>
+			<h1 className='text-4xl text-white font-bold mb-5 flex items-center'>
+				Memory Game{' '}
+				<CircleHelp
+					className='ml-5 hover:text-black cursor-pointer'
+					onClick={handleHelpClick}
+				/>
+			</h1>
+			<Help display={showHelp} />
+			<div className='flex'>
+				{id.map((index) => {
+					return (
+						<Card
+							key={index}
+							id={index}
+							onClick={() => handleCardClick(index)}
+						/>
+					)
+				})}
+			</div>
+			<div className='flex text-xl text-white mt-10'>
+				<p>Score: {score}</p>
+				<p className='ml-5'>Best Score: {bestScore}</p>
+			</div>
+			<LoseAlert lose={lose} score={prevScore} newGame={newGame} />
+			<WinAlert win={clickedCards.length == 5} />
+		</div>
+	)
 }
 
 export default App
--- src/components/LoseAlert.jsx
+++ src/components/LoseAlert.jsx
@@ -1,8 +1,12 @@
 export default function LoseAlert(props) {
     if(props.lose){
         return(
-            <div className="absolute flex items-center justify-center bg-blue-950 border-4 w-150 h-100">
-                <p className="text-xl text-white">You lose. Your score was {props.score}!</p>
+            <div className="absolute flex flex-col items-center justify-center bg-blue-950 border-4 w-150 h-100">
+                <p className="text-xl text-white mb-5">You lose. Your score was {props.score}!</p>
+                <div className="flex justify-center text-white">
+                    <button className="disabled bg-red-600 w-20 h-15 cursor-not-allowed hover:bg-red-700">No</button>
+                    <button className="bg-green-600 w-20 h-15 hover:bg-green-700 cursor-pointer" onClick={props.newGame}>Yes</button>
+                </div>
             </div>
         )
     }
