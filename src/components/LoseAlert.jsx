c1d7f9d070ece7256d0d2f79e2014c1aa7182090
N3nT <bar55v71@interia.pl>
2025-01-25 16:35:27 +0100

    Easter egg

--- src/components/LoseAlert.jsx
+++ src/components/LoseAlert.jsx
@@ -4,7 +4,7 @@ export default function LoseAlert(props) {
             <div className="absolute flex flex-col items-center justify-center bg-blue-950 border-4 w-150 h-100">
                 <p className="text-xl text-white mb-5">You lose. Your score was {props.score}!</p>
                 <div className="flex justify-center text-white">
-                    <button className="disabled bg-red-600 w-20 h-15 cursor-not-allowed hover:bg-red-700">No</button>
+                    <a href="https://github.com/N3nT" target="blank_" className="disabled bg-red-600 w-20 h-15 cursor-not-allowed hover:bg-red-700 text-center flex items-center justify-center">No</a>
                     <button className="bg-green-600 w-20 h-15 hover:bg-green-700 cursor-pointer" onClick={props.newGame}>Yes</button>
                 </div>
             </div>
